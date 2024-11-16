07db5b52d1b3a5e0ae62eba77b07cb3525a29750
Bryan Patrick <=>
2024-11-16 16:43:50 -0300

    add: composables

---
+++ composables/index.js
@@ -0,0 +1,19 @@
+export const useAgora = () => useState("teste", () => ["laranja", "coco"]);
+
+export const getters = {};
+
+export const mutations = {};
+
+export const actions = {};
+
+//     user1: 'Bryan Patrick',
+//     user2: 'Elmo',
+//     user3: 'Felipe',
+//     user4: 'Vinicius',
+//     user5: 'Cristiano',
+//     time1: '07:00 às 19:00',
+//     time2: '19:00 às 07:00',
+//     color1: "background-color: rgb(227, 240, 215)",
+//     color2: "background-color: rgb(229, 213, 230)",
+//     color3: "background-color: rgb(252, 229, 212)",
+//     color4: "background-color: rgb(225, 240, 200)",
